47612777944d6db720a7a0ef40f2095d5b1a4e55
Terajoel <jdejesus@teravisiongames.com>
2019-10-19 17:13:15 -0500

    Minor improvments and testing on the fractal !

--- AdvCPPCourse/7_FractalProject/main.cpp
+++ AdvCPPCourse/7_FractalProject/main.cpp
@@ -10,24 +10,32 @@ int main()
 
 	FractalCreator fractalMaker(_width, _height);
 
-	fractalMaker.AddRange(0.0, FRGB(0,0,0));
-	fractalMaker.AddRange(0.05, FRGB(255,0,0));
-	fractalMaker.AddRange(0.1, FRGB(0,255,0));
-	fractalMaker.AddRange(1.0, FRGB(0,0,255));
+	fractalMaker.AddRange(0.0, FRGB(255,255,0));
+	fractalMaker.AddRange(0.1, FRGB(0,0,255));
+	fractalMaker.AddRange(0.2, FRGB(0,255,0));
+	fractalMaker.AddRange(0.3, FRGB(255,0,0));
+	fractalMaker.AddRange(0.4, FRGB(0,255,0));
+	fractalMaker.AddRange(0.5, FRGB(0,0,255));
+	fractalMaker.AddRange(0.6, FRGB(0,255,0));
+	fractalMaker.AddRange(0.7, FRGB(255,0,0));
+	fractalMaker.AddRange(0.8, FRGB(0,255,0));
+	fractalMaker.AddRange(0.9, FRGB(0,0,255));
+	fractalMaker.AddRange(1.0, FRGB(0,255,0));
 /*
 	fractalMaker.AddRange(0.0, FRGB(0,0,0));
 	fractalMaker.AddRange(0.3, FRGB(255,0,0));
 	fractalMaker.AddRange(0.5, FRGB(255,0,0));
 	fractalMaker.AddRange(1.0, FRGB(255,0,0));
 */
+
 	fractalMaker.AddZoom(FZoom(_width/2, _height/2, 4.0/_width)); // 4.0/_width is a nice zoom to see the fractal
 	fractalMaker.AddZoom(FZoom(295, _height-202, 0.1));
 	fractalMaker.AddZoom(FZoom(312, _height-304, 0.1));
-	//fractalMaker.AddZoom(FZoom(181, _height-289, 0.1));
+	fractalMaker.AddZoom(FZoom(181, _height-289, 0.1));
+	fractalMaker.AddZoom(FZoom(_width/2, _height/2, 0.1));
+	fractalMaker.AddZoom(FZoom(_width/2, _height/2, 0.1));
 
-	std::cout << fractalMaker.GetRange(300) << std::endl;
-
-	fractalMaker.Run("fractal3.bmp");
+	fractalMaker.Run("fractal9.bmp");
 
 	return 0;
 }
